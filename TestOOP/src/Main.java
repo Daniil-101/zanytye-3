b2b2a9e1013b2b1d26debaf54e7b8a08f7475884
Daniil-101 <kirden98@gmail.com>
2023-02-20 13:17:21 +0300

    Second commit

--- TestOOP/src/Main.java
+++ TestOOP/src/Main.java
@@ -5,8 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Тест по Java."); // Вводные слова
-        System.out.println("В тесте всего 3 вопроса, для прохождения данного теста нужно правильно ответить на 2.");
+        System.out.println("Тест по Java.");
 
         String[] questions = {"Начнём с простого: что такое Java?",
                 "Что из следующего является диапазоном char в Java?",
@@ -18,22 +17,9 @@ public class Main {
 
         int[] correntAnswersIndexes = {3, 3, 1}; // Номера правильных ответов
 
-        int counter = 0; // Счетчик верных ответов
 
-        for (int i = 0; i < answers.length; i++) {
-            Question.typeQuestions(i, questions);
-            Answers.typeAnswers(i, answers);
-            int userResponse = sc.nextInt();
-            boolean correctOrNot = Answers.correctAnswerOrNot(i, userResponse, correntAnswersIndexes);
-            if (correctOrNot == true) {
-                System.out.println("Верно");
-                counter++;
-            } else {
-                System.out.println("Неверно");
-            }
 
-        }
-        System.out.println("Вы ответили верно на " + counter);
-        System.out.println(counter >= 2 ? "Ты молодец! Тест пройден." : "Ничего страшного, попробуй еще раз.");
+        Question.passTest(questions, answers, correntAnswersIndexes);
+
     }
 }
--- TestOOP/src/Question.java
+++ TestOOP/src/Question.java
@@ -1,13 +1,46 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Question {
-
+    boolean correctAnswerOrNot;
     public int number;
     public String questions;
+    static Scanner sc = new Scanner(System.in);
+    static int count = 0;
 
 
-    public static void typeQuestions(int number, String[] questions) {
-        System.out.println(questions[number]);
+    public static void passTest(String[] questions, String[][] answers, int[] textAnswer) {
+        for (int i=0; i<answers.length; i++) {
+            System.out.println(questions[i]);
+            for (int j = 0; j < answers[i].length; j++) {
+                System.out.println(answers[i][j]);
+            }
+            System.out.print("Ваш ответ: ");
+            int userResponse = sc.nextInt();
+            boolean result = correctAnswerOrNot(i, userResponse, textAnswer);
+            if (result == true) {
+                count++;
+                System.out.println("Верно");
+            } else {
+                System.out.println("Неверно");
+            }
+        }
+        System.out.println("Вы ответили верно на " + count);
+        System.out.println(count >= 2 ? "Ты молодец! Тест пройден." : "Ничего страшного, попробуй еще раз.");
     }
 
+    public static boolean correctAnswerOrNot(int numberAnswer, int userResponse, int[] textAnswer) {
+        if (userResponse == textAnswer[numberAnswer]) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /*for (int i = 0; i < answers.length; i++) {
+        Question.typeQuestions(i, questions);
+        Answers.typeAnswers(i, answers);
+        int userResponse = sc.nextInt();
+        boolean correctOrNot = Answers.correctAnswerOrNot(i, userResponse, correntAnswersIndexes);
+*/
 }
